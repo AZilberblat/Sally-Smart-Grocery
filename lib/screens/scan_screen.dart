c05fbd77ca5b1b26d4f85489e7110692f63022ce
Abraham Zilberblat <39259098+AZilberblat@users.noreply.github.com>
2019-08-05 15:49:45 +0300

    added barcode scanner

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/services.dart';
 
 class ScanScreen extends StatefulWidget {
   static const String id = 'scan_screen';
@@ -8,7 +10,64 @@ class ScanScreen extends StatefulWidget {
 
 class _ScanScreenState extends State<ScanScreen> {
   @override
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver("#ff6666", "Cancel", true)
+        .listen((barcode) => print(barcode));
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes =
+          await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Barcode scan')),
+            body: Builder(builder: (BuildContext context) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton(
+                            onPressed: () {
+                              initPlatformState();
+                            },
+                            child: Text("Start barcode scan")),
+                        Text('Scan result : $_scanBarcode\n',
+                            style: TextStyle(fontSize: 20)),
+                        RaisedButton(
+                            onPressed: () {
+                              startBarcodeScanStream();
+                            },
+                            child: Text("Start barcode scan stream"))
+                      ]));
+            })));
   }
 }
